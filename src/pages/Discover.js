2e954c1e182f0f919064d13ef11133a307923b2b
Shunze Lin <4A490101@stust.edu.tw>
2021-03-26 09:38:15 +0800

    feat:Building initial Book screen

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -45,6 +45,7 @@ function Discover() {
 					css={`
 						border: solid 1px rgba(200, 200, 200, 0.7);
 						border-radius: 5px;
+						position: relative;
 						&:hover {
 							box-shadow: 1rem 1rem 1rem rgba(200, 200, 200, 0.7);
 						}
@@ -123,10 +124,48 @@ function Discover() {
 							</p>
 						</div>
 					</Link>
+					<div
+						css={`
+							position: absolute;
+							top: 0;
+							width: 5rem;
+							height: 100%;
+							right: -2.5rem;
+							display: flex;
+							flex-direction: column;
+							align-items: center;
+							justify-content: space-around;
+						`}
+					>
+						<button
+							css={`
+								z-index: 11;
+								position: relative;
+								color: var(--colors-text--main);
+								font-size: 1.7rem;
+								background: none;
+								border: solid 1px rgba(200, 200, 200, 0.7);
+								padding: 1rem;
+								border-radius: 50%;
+								display: flex;
+								align-items: center;
+								&:hover {
+									color: #3f5eb5;
+									cursor: pointer;
+								}
+								&:focus {
+									outline: none;
+								}
+							`}
+						>
+							<i className='fas fa-plus-circle'></i>
+						</button>
+					</div>
 				</li>
 			</ul>
 		</div>
 	);
 }
-
+//<i class="fas fa-check-circle"></i>
+//<i class="fas fa-minus-circle"></i>
 export default Discover;
